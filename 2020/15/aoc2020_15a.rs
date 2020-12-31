728bb10f16a09c45a01e5f92eae1ae0d0470ebdc
vole-dev <dev@vole.dev>
2020-12-31 16:27:38 -0600

    aoc 2020 day 15

---
+++ 2020/15/aoc2020_15a.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::io::Result;
+use std::collections::HashMap;
+
+fn main() -> Result<()> {
+    let lines: Vec<String> = BufReader::new(File::open("input.txt")?).lines().map(|l| l.unwrap()).collect();
+    assert!(lines.len() == 1);
+    let numbers: Vec<i64> = lines[0].split(',').map(|ns| ns.parse().unwrap()).collect();
+
+    let mut record: HashMap<i64, usize> = HashMap::new();
+    for (i, &n) in numbers[0..numbers.len()-1].iter().enumerate() {
+        record.insert(n, i);
+    }
+
+    let mut last_value = numbers[numbers.len()-1];
+
+    for i in numbers.len()-1..2019 {
+        let new_value = if let Some(&last_idx) = record.get(&last_value) {
+            (i - last_idx) as i64
+        } else {
+            0
+        };
+        record.insert(last_value, i);
+        last_value = new_value;
+    }
+
+    println!("2020th spoken number: {}", last_value);
+
+    Ok(())
+}
